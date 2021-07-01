b43c48d67e9a3f5ea7a91806f15f7e80f78155d2
Matheus de Araujo <matheus.encode@gmail.com>
2021-07-01 20:17:34 -0300

    commit inicial

---
+++ src/routes.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+} from 'react-router-dom';
+
+import Home from './pages/Home';
+import Login from './pages/Login';
+
+const Routes = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route path="/login" component={Login} />
+      <Route path="/" component={Home} />
+    </Switch>
+  </BrowserRouter>
+)
+
+
+export default Routes;
