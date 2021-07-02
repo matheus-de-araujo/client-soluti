1d5c663acbf2fd4ef2518fd6f3185a8a3e424d2d
Matheus de Araujo <matheus.encode@gmail.com>
2021-07-01 21:04:06 -0300

    config inicial

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import React from 'react';
+import { Router } from 'react-router-dom';
+
+
+import history from './history';
 import Routes from './routes';
+
 function App() {
   return (
+    <Router history={history}>
     <Routes />
+    </Router>
   );
 }
 
